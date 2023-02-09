cfe4a75a0c17a1b5224612f77909e10f3bbb2129
albertobaggio96 <albertobaggio96@gmail.com>
2023-02-09 17:21:43 +0100

    made some css

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,25 +1,18 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+@extends("layouts.app")
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel</title>
-
-    <!-- Fonts -->
-    <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-
-    <!-- Styles -->
-    @vite('resources/js/app.js')
-
-</head>
-
-<body>
-    <h1>hello</h1>
-    @foreach($movies as $movie)
-        <p>{{$movie}}</p>
-    @endforeach
-</body>
-
-</html>
+@section("main")
+    <section id="maive-card" class="container">
+        <div class="row row-cols-6 text-center">
+            @foreach($movies as $movie)
+            <article class="col bg-info m-3">
+                <h2 class="fs-3">{{$movie->title}}</h2>
+                <h6>{{$movie->original_title}}</h6>
+                <div>{{$movie->nationality}}</div>
+                <div>{{$movie->date}}</div>
+                <div>{{$movie->vote}}</div>
+            </article>
+            @endforeach
+        </div>
+    </section>
+@endsection
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,1 @@
+<h1 class="text-center pb-5">MOVIE</h1>
